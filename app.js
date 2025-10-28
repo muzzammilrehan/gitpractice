0c280e87120208225845c9bc18bcdc3a343fd923
muzzammil <muzzammilrehan2007@gmail.com>
2025-10-27 22:50:22 -0700

    all changes

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ $(document).ready(function(){
         url : page + '.html',
         success : function(data){
             $('#content').html(data)
+            if(page == 'home'){
+                dataFetching()
+            }
         }
 
     })
 
 }
 
-$("#index").click(function(e){
+$("#home").click(function(e){
 e.preventDefault();
-loadPage('index')
+loadPage('home')
 })
 
 $("#about").click(function(e){
@@ -37,27 +40,27 @@ e.preventDefault();
 loadPage('services')
 })
 
-loadPage("index")
+loadPage("home")
 
 // data fetching
 
 function dataFetching() {
     $.ajax({
-        url: 'data json',
+        url: 'data.json',
         type: 'get',
-        sucess: function (users) {
+        success: function(users) {
 
             let myhtml = ""
             users.forEach(users => {
 
                 myhtml += `
-              <div class="container">
-        <div class="row">
+            
+      
             <div class="col-4">
                 <div class="card" style="width: 18rem;">
                   <img src="${users.p_image}" alt="...".>
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
+    <h5 class="card-title">${users.p_title}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
@@ -70,8 +73,13 @@ function dataFetching() {
 
                 
             })
+
+            $("#myrow").html(myhtml);
         }
 })
+
 }
+
+dataFetching()
 
 })
